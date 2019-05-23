9465a07f7f39fce93184b1303f7d35ac91bdf880
ahalyabaskaran <43839106+ahalyabaskaran@users.noreply.github.com>
2019-05-23 14:29:32 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,5 @@
+ahal=input()
+if(ahal=='a'or ahal=='e'or ahal=='i'or ahal=='o'or ahal=='u'or ahal=='A'or ahal=='E'or ahal=='I'or ahal=='O'or ahal=='U'):
+  print("Vowel")
+else:
+  print("Consonant")
